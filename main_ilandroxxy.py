9865bb220b1df7f12d00dde5f7c0c4c98a48ecf5
Ilya Andrianov <ilandroxxy@gmail.com>
2023-06-25 17:58:14 +0700

    Выполнил задачу 1 из Урока 3:
    - Функция /give с стикером и текстом
    
    - Отвечаем на красное сердечко - черным

--- main_ilandroxxy.py
+++ main_ilandroxxy.py
@@ -1,32 +1,33 @@
+# region import-ы
 from aiogram import Bot, Dispatcher, executor, types
 import os
 from dotenv import load_dotenv
-
-PARSE_MODE: str = 'HTML'
-COUNT = 0
-
 
 load_dotenv()
 token = os.getenv('TOKEN_API')
 
 bot = Bot(token)
 dp = Dispatcher(bot)
+# endregion import-ы
+
+PARSE_MODE: str = 'HTML'
 
 
-@dp.message_handler(commands=['count'])
+async def on_startup(_):
+    print('Бот был успешно запущен!')
+
+
+@dp.message_handler(commands=['give'])
 async def count_command(message: types.Message):
-    global COUNT
-    COUNT += 1
-    await message.answer(text=f"Кол-во запусков: {COUNT}")
+    await message.answer(text="Смотри, какой смешной кодер 👾")
+    await bot.send_sticker(message.from_user.id, sticker="CAACAgQAAxkBAAEJeRlkmBvuHYXoPXPqA1ZaRA6XpZuTngACUAEAAqghIQaxvfG1zemEoi8E")
 
 
 @dp.message_handler()
-async def echo(message: types.Message):
-    if "0" in message.text:
-        await message.reply(text="YES")
-    else:
-        await message.reply(text='NO')
+async def echo_emoji(message: types.Message):
+    if message.text == "❤️":
+        await message.reply(text="🖤")
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, on_startup=on_startup)
