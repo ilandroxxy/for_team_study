bc30727776490e8b3204127d0816dee8e0433625
Ilya Andrianov <ilandroxxy@gmail.com>
2023-06-25 17:37:47 +0700

    Решил вторую задачу из Урока 2 AIOGRAM:
    - Функция /count выводящая кол-во запусков команды через глобальную переменную
    
    - Если в тексте есть 0, то писать YES, иначе NO

--- main_ilandroxxy.py
+++ main_ilandroxxy.py
@@ -1,17 +1,10 @@
 from aiogram import Bot, Dispatcher, executor, types
 import os
-from random import choice
-from string import ascii_uppercase
 from dotenv import load_dotenv
 
 PARSE_MODE: str = 'HTML'
-ALPHABET = ascii_uppercase
+COUNT = 0
 
-HELP_TEXT = """
-/start - перезапустить бот
-/help - список команд в боте
-/description - описание бота
-"""
 
 load_dotenv()
 token = os.getenv('TOKEN_API')
@@ -20,26 +13,19 @@ bot = Bot(token)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
-    await message.answer(text="Hello, World!")
-    await message.delete()
-
-
-@dp.message_handler(commands=['help'])
-async def help_command(message: types.Message):
-    await message.reply(text=HELP_TEXT)
-
-
-@dp.message_handler(commands=['description'])
-async def description_command(message: types.Message):
-    await message.answer(text="Пока что это просто учебный бот\n"
-                              "Попробуем сделать что-то интересное.")
+@dp.message_handler(commands=['count'])
+async def count_command(message: types.Message):
+    global COUNT
+    COUNT += 1
+    await message.answer(text=f"Кол-во запусков: {COUNT}")
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    await message.reply(text=choice(ALPHABET))
+    if "0" in message.text:
+        await message.reply(text="YES")
+    else:
+        await message.reply(text='NO')
 
 
 if __name__ == '__main__':
