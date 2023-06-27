837a1597e2940bc77593b39c8083fdb4e119f447
Ilya Andrianov <ilandroxxy@gmail.com>
2023-06-27 23:37:26 +0700

    Выполнил 1 задачу из практики к уроку по клавиатурам:
    - Создал клавиатуру с командами /help и /description
    
    - При отправлении середчка, бот ответит стикером

--- main_ilandroxxy.py
+++ main_ilandroxxy.py
@@ -1,5 +1,6 @@
 # region import-ы
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 import os
 from dotenv import load_dotenv
 
@@ -16,13 +17,42 @@ HELP_TEXT = """
 <b>/help</b> - <em>вывести список команд</em>
 """
 
+kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+btn1 = KeyboardButton("/help")
+btn2 = KeyboardButton("/description")
+btn3 = KeyboardButton("❤️")
+kb.add(btn1, btn2)
+
+
 async def on_startup(_):
     print('Бот был успешно запущен!')
 
 
 @dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
-    await message.answer(text=HELP_TEXT, parse_mode="HTML")
+    await message.answer(text=HELP_TEXT, parse_mode="HTML", reply_markup=kb)
+
+
+@dp.message_handler(commands=['clear'])
+async def clear_command(message: types.Message):
+    await bot.send_message(message.chat.id, text="Удалил кнопки", reply_markup=ReplyKeyboardRemove())
+
+
+@dp.message_handler(commands=['getmyid'])
+async def getmyid_command(message: types.Message):
+    await bot.send_message(message.chat.id, text=f"Ваш id: {message.chat.id}")
+
+
+@dp.message_handler(commands=['order'])
+async def order_command(message: types.Message):
+    if message.chat.id == 438879394:
+        await message.answer(text="Ты даун, зачем заказал сам у себя!")
+    else:
+        await bot.send_message(438879394, text=f"Был оформлен заказ: {message.chat.id}")
+        await message.answer(text="Заказ успешно отправлен!")
+        await message.delete()
+        await bot.send_sticker(message.chat.id, sticker="CAACAgQAAxkBAAEJeRlkmBvuHYXoPXPqA1ZaRA6XpZuTngACUAEAAqghIQaxvfG1zemEoi8E")
+
 
 
 @dp.message_handler(commands=['location'])
@@ -34,7 +64,10 @@ async def send_point(message: types.Message):
 
 @dp.message_handler()
 async def echo_emoji(message: types.Message):
-    await message.reply(text=f"В твоем сообщение {message.text.count('✅')} символов ✅.")
+    if message.text == "❤️":
+        await bot.send_sticker(message.chat.id, sticker="CAACAgIAAxkBAAEJgENkmw_PBJxS83dlUsd_TtPiZFfRWgACERQAAqAAAehLhynfNnamXaEvBA")
+    else:
+        await message.reply(text=f"В твоем сообщение {message.text.count('✅')} символов ✅.")
 
 
 @dp.message_handler(content_types=['sticker'])
