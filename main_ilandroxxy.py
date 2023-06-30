8c33b0f091de97fd6fbdfa495599ec1e3f48b98a
Ilya Andrianov <ilandroxxy@gmail.com>
2023-07-01 02:01:58 +0700

    Модифицировал файл `main_ilandroxxy`:
    - Вынес клавиатуры типа ReplayMarkup в отдельный файл keyboard_handlers
    
    - Вынес inline клавиатуры в отдельный файл inline_handlers

--- main_ilandroxxy.py
+++ main_ilandroxxy.py
@@ -1,7 +1,10 @@
 # region import-ы
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters import Text
+
 from inline_handlers import links_buttons, contact_buttons
-from keyboard_handlers import kb
+from keyboard_handlers import kb, kb_send_photo
+
 import os
 import random
 from dotenv import load_dotenv
@@ -27,11 +30,27 @@ async def start_command(message: types.Message):
 async def links_command(message: types.Message):
     await message.answer(text="Давайте решим рандомную задачку", reply_markup=links_buttons)
 
+@dp.message_handler(commands=["Venya"])
+async def Venya_coommand(message: types.Message):
+    await message.answer(text="<b>Веня</b>, мы тебя услышали", parse_mode='HTML')
+    await bot.send_sticker(message.chat.id, sticker="CAACAgIAAxkBAAEJhmBknVKv0Thaglc40AabNwk4rLRQ9gAC_wEAApfYKgz93kVX-B64Pi8E")
+    await message.delete()
+
+
+
+@dp.message_handler(Text(equals='Рандомная картинка'))
+async def open_keyboard_photo(message: types.Message):
+    await message.answer(text="Чтобы отправить рандомную фотографию нажми на кнопку Рандом",
+                         reply_markup=kb_send_photo)
+
 
 @dp.message_handler()
 async def echo_answer(message: types.Message):
     if message.text == 'Контакты':
         await bot.send_message(message.chat.id, text="А вот нажми на кнопочку", reply_markup=contact_buttons)
+    elif message.text == 'Главное меню':
+        await message.answer(text='Открыл главное меню', reply_markup=kb_send_photo)
+        await message.delete()
 
 
 @dp.callback_query_handler()
