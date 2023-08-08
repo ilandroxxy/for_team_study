bfdb384a3fee76d0681f719c6da7e1a7a01234ac
Grimstrou <mr.recgyn@mail.ru>
2023-08-09 01:57:46 +0700

    прописали текста в хендлере

---
+++ text_handlers.py
@@ -0,0 +1,37 @@
+import random
+
+
+class ForUser():
+    def __int__(self, message):
+        self.message = message
+
+    @staticmethod
+    def push_command_start(self):
+        text = f'Добро пожаловать, {self.from_user.first_name}'
+        return text
+
+    @staticmethod
+    def push_command_getmyid(self):
+        text = f'Вот ваш ID: `{self.chat.id}`'
+        return text
+
+    @staticmethod
+    def push_trash(self):
+        answer = ('Что тут вообще происходит?', 'Что здесь происходит?', 'Что тут происходит?',
+                  'Что тут вообще творится?', 'Что здесь вообще происходит?', 'Что здесь творится?',
+                  'Что тут вообще творится?', 'Что здесь происходит?', 'Что тут происходит?',
+                  'Что здесь вообще творится?')
+        return random.choice(answer)
+
+
+
+class ForAdmin():
+    def __int__(self, message):
+        self.message = message
+
+    @staticmethod
+    def send_help_self(self, mess):
+        text = f'#help Пользователь: @{self.from_user.username}\n' \
+               f'Пишет сообщение: {mess}'
+        return text
+
