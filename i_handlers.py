ee20ffcd7b33cfd75d8750845548c5e1f96c2135
SSSMP <mustrax@yandex.ru>
2023-08-16 20:47:56 +0700

    Добавил для команды 'контакты' inlinekeyboard

--- i_handlers.py
+++ i_handlers.py
@@ -1,26 +1,30 @@
+"""В этом файле лежит кнопка пересылки айдишника админу"""
+
+
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from texts_handlers import *
 
 def get_forward_id(mess) -> InlineKeyboardMarkup:
     ikb = InlineKeyboardMarkup(inline_keyboard=[
         [
-        InlineKeyboardButton(text='Vashemu repu',
+        InlineKeyboardButton(text=ForUser.push_ikbtext_getmyid(mess),
                              switch_inline_query=ForUser.push_command_getmyid(mess))
         ]
     ])
     return ikb
 
 
-'''
-def get_contact() -> InlineKeyboardMarkup:
+
+def get_contact_github(mess) -> InlineKeyboardMarkup:
     ikb = InlineKeyboardMarkup(inline_keyboard=[
         [
-        InlineKeyboardButton(text='GitHub', url='https://github.com/SMP1488'),
-        InlineKeyboardButton(text='Tg', url='t.me/AnotherPumpkin'),
+        InlineKeyboardButton(text=ForUser.push_ikbtext_contact_github(mess), url='https://github.com/SMP1488'),
+        InlineKeyboardButton(text=ForUser.push_ikbtext_contact_tg(mess), url='t.me/AnotherPumpkin')
         ]
     ])
     return ikb
-'''
+
+
 
 
 
